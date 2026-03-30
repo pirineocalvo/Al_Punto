273e472faa3f5d8fc9f42b047d28ea5f6ffe3e62
Sergio Salinas <correoSergio@falso.com>
2026-03-30 17:02:55 +0200

    feat(frontend): estilos footer

--- frontend/src/Components/cabeceraYpiePrincipal/Footer.vue
+++ frontend/src/Components/cabeceraYpiePrincipal/Footer.vue
@@ -1,32 +1,33 @@
 <template>
   <footer id="preFooter">
     <a-row>
-      <a-col :span="6">
-        <a-typography-title :level="4" class="">Al Punto</a-typography-title>
-        <a-typography-paragraph class="footer-desc">Cocina honesta y producto fresco desde
-          1994.</a-typography-paragraph>
+      <a-col :xs="24" :md="12" :xl="6">
+        <a-typography-title class="titulosFooter" :level="4">Al Punto</a-typography-title>
+        <a-typography-paragraph class="txtFooter">Cocina honesta y producto fresco desde 1994.</a-typography-paragraph>
       </a-col>
-      <a-col :span="6">
-        <a-typography-title :level="5" class="">Horario</a-typography-title>
-        <div class=""><span>Lunes – Jueves</span><span class="">13:00 – 22:00</span></div>
-        <div class=""><span>Viernes – Sábado</span><span class="">13:00 – 23:00</span></div>
-        <div class=""><span>Domingo</span><span class="">13:00 – 21:00</span></div>
+      <a-col :xs="24" :md="12" :xl="6">
+        <a-space direction="vertical">
+          <a-typography-title class="titulosFooter" :level="4">Horario</a-typography-title>
+          <a-typography-text class="txtFooter">Lunes – Jueves 13:00 – 22:00</a-typography-text>
+          <a-typography-text class="txtFooter">Viernes – Sábado 13:00 – 23:00</a-typography-text>
+          <a-typography-text class="txtFooter">Domingo 13:00 – 21:00</a-typography-text>
+        </a-space>
       </a-col>
-      <a-col :span="6">
-        <a-typography-title :level="5" class="">Contacto</a-typography-title>
+      <a-col :xs="24" :md="12" :xl="6">
+        <a-typography-title class="titulosFooter" :level="4">Contacto</a-typography-title>
         <a-space direction="vertical" size="small">
-          <a-typography-text class="">Calle Mayor 12, Salamanca</a-typography-text>
-          <a-typography-text class="">923 000 000</a-typography-text>
-          <a-typography-text class="">hola@alpunto.es</a-typography-text>
+          <a-typography-text class="txtFooter">Calle Mayor 12, Salamanca</a-typography-text>
+          <a-typography-text class="txtFooter" copyable>923 000 000</a-typography-text>
+          <a-typography-text class="txtFooter" copyable>hola@alpunto.es</a-typography-text>
         </a-space>
       </a-col>
     </a-row>
     <a-row>
-      <a-col :span="6">
-        <a-flex>
-          <a-typography-link class="">Privacidad</a-typography-link>
-          <a-typography-link class="">Aviso legal</a-typography-link>
-        </a-flex>
+      <a-col>
+        <a-typography-link>Privacidad</a-typography-link>
+      </a-col>
+      <a-col>
+        <a-typography-link>Aviso legal</a-typography-link>
       </a-col>
     </a-row>
   </footer>
