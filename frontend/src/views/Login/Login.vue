640886b0a20f1404569dce559263967da79d8748
Sergio Salinas <correoSergio@falso.com>
2026-03-31 22:02:39 +0200

    feat(frontend): responsibidad en login

--- frontend/src/views/Login/Login.vue
+++ frontend/src/views/Login/Login.vue
@@ -1,32 +1,3 @@
-<template>
-    <section class="contenedorLogin">
-        <a-form :model="formState" :rules="rules" @finish="verificarUser()">
-            <a-form-item label="Correo electrónico" name="email" :label-col="{ span: 7 }" :wrapper-col="{ span: 12 }">
-                <a-input v-model:value="formState.email" placeholder="Correo electrónico">
-                    <template #prefix>
-                        <UserOutlined />
-                    </template>
-                </a-input>
-            </a-form-item>
-
-            <a-form-item label="Contraseña" name="password" :label-col="{ span: 7 }" :wrapper-col="{ span: 12 }">
-                <a-input-password v-model:value="formState.password" placeholder="Contraseña">
-                    <template #prefix>
-                        <LockOutlined />
-                    </template>
-                </a-input-password>
-            </a-form-item>
-
-            <a-form-item :wrapper-col="{ span: 12, offset: 5 }">
-                <div class="separarBtn">
-                    <a-button class="btnPrincipal" type="primary" html-type="submit">Iniciar sesión</a-button>
-                    <a-button class="btnSecundario" @click="volver">Volver</a-button>
-                </div>
-            </a-form-item>
-        </a-form>
-    </section>
-</template>
-
 <script setup>
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
@@ -59,7 +30,7 @@ const rules = {
 }
 
 async function verificarUser() {
-        try {
+    try {
         await loginUser(formState.value)
         router.push('/')
     } catch (error) {
@@ -71,3 +42,34 @@ const volver = () => {
     router.back()
 }
 </script>
+
+<template>
+    <div class="contenedorLogin">
+        <a-card class="cardLogin">
+            <a-form :model="formState" :rules="rules" @finish="verificarUser()">
+                <a-form-item label="Correo electrónico" name="email" :label-col="{ span: 7 }" :wrapper-col="{ span: 12 }">
+                    <a-input v-model:value="formState.email" placeholder="Correo electrónico">
+                        <template #prefix>
+                            <UserOutlined />
+                        </template>
+                    </a-input>
+                </a-form-item>
+
+                <a-form-item label="Contraseña" name="password" :label-col="{ span: 7 }" :wrapper-col="{ span: 12 }">
+                    <a-input-password v-model:value="formState.password" placeholder="Contraseña">
+                        <template #prefix>
+                            <LockOutlined />
+                        </template>
+                    </a-input-password>
+                </a-form-item>
+
+                <a-form-item :wrapper-col="{ span: 12, offset: 5 }">
+                    <div class="separarBtn">
+                        <a-button class="btnPrincipal" type="primary" html-type="submit">Iniciar sesión</a-button>
+                        <a-button class="btnSecundario" @click="volver">Volver</a-button>
+                    </div>
+                </a-form-item>
+            </a-form>
+        </a-card>
+    </div>
+</template>
