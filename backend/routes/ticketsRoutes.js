a21f6e541a5140f55a36177a978a2816957032c8
demoniershome <Equipo@deminierspcserver.es>
2026-04-07 21:14:18 +0200

    parche back

--- backend/routes/ticketsRoutes.js
+++ backend/routes/ticketsRoutes.js
@@ -1,167 +1,164 @@
-const express=require('express'),router=express['Router'](),multer=require('multer'),path=require('path'),fs=require('fs'),Tesseract=require('tesseract.js'),{
-decrypt
+const express = require('express'), router = express['Router'](), multer = require('multer'), path = require('path'), fs = require('fs'), Tesseract = require('tesseract.js'), {
+    decrypt
 }
-=require('../utils/crypto'),db=require('../utils/db.js'),storage=multer['diskStorage']({
-'destination':(a,b,c)=>{
-const d=path['join'](__dirname,'../uploads/tickets');
-!fs['existsSync'](d)&&fs['mkdirSync'](d,{
-'recursive':!![]
-}
-),c(null,d);
+    = require('../utils/crypto'), db = require('../utils/db.js'), storage = multer['diskStorage']({
+        'destination': (a, b, c) => {
+            const d = path['join'](__dirname, '../uploads/tickets');
+            !fs['existsSync'](d) && fs['mkdirSync'](d, {
+                'recursive': !![]
+            }
+            ), c(null, d);
+
+        }
+        , 'filename': (a, b, c) => {
+            try {
+                const d = a['headers']['authorization'], e = d?.['split']('\x20')[0x1], f = decrypt(e), g = new Date(), h = g['getFullYear'](), i = String(g['getMonth']() + 0x1)['padStart'](0x2, '0'), j = String(g['getDate']())['padStart'](0x2, '0'), k = String(g['getHours']())['padStart'](0x2, '0'), l = String(g['getMinutes']())['padStart'](0x2, '0'), m = String(g['getSeconds']())['padStart'](0x2, '0'), n = '' + h + i + j + k + l + m, o = n + '_' + f + '.jpg';
+                a['generatedFileName'] = o, c(null, o);
+
+            }
+            catch (p) {
+                console['error']('Error\x20generando\x20nombre\x20de\x20archivo:', p), c(p);
+
+            }
+
+        }
+
+    }
+    ), upload = multer({
+        'storage': storage
+    }
+    ), analyzeTicket = async a => {
+        try {
+            const {
+                data: {
+                    text: b
+                }
+
+            }
+                = await Tesseract['recognize'](a, 'spa', {
+                    'logger': c => console['log'](c)
+                }
+                );
+            return b;
+
+        }
+        catch (c) {
+            console['error']('Error\x20en\x20OCR:', c);
+            throw new Error('Error\x20al\x20procesar\x20la\x20imagen\x20con\x20OCR');
+
+        }
+
+    }
+    , calcularPuntos = a => {
+        let b = 0x0;
+        const c = /Total:\s*.*?(\d+(?:[.,]\d{1,2})?)\s*€/i;
+        return d ? b = parseFloat(d[0x1]) * 0x64 : b = 0x0, b;
+
+    }
+    ;
+router['post']('/upload', upload['single']('imagen'), async (a, b) => {
+    const c = a['headers']['authorization'];
+    if (!c || !c['startsWith']('Bearer\x20')) return b['status'](0x191)['json']({
+        'error': 'Token\x20no\x20proporcionado\x20o\x20formato\x20inválido'
+    }
+    );
+    const d = c['split']('\x20')[0x1], e = decrypt(d);
+    if (!e) return b['status'](0x191)['json']({
+        'error': 'Token\x20inválido'
+    }
+    );
+    if (!a['file']) return b['status'](0x190)['json']({
+        'error': 'No\x20se\x20ha\x20subido\x20ninguna\x20imagen'
+    }
+    );
+    const f = a['file']['path'], g = a['generatedFileName'];
+    try {
+        const h = await analyzeTicket(f), i = calcularPuntos(h);
+        let j = 'ok';
+        if (i === 0x0) j = 'review';
+        const k = {
+            'userId': e, 'fileName': g, 'text': h, 'points': i
+        }
+            ;
+        db['run']('INSERT\x20INTO\x20Tickets\x20(user_id,\x20image_url,\x20json_content,\x20points_awarded,\x20status)\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20VALUES\x20(?,\x20?,\x20?,\x20?,\x20?)', [k['userId'], k['fileName'], k['text'], k['points'], j], function (l) {
+            if (l) return b['status'](0x1f4)['json']({
+                'error': 'Error\x20al\x20insertar\x20ticket'
+            }
+            );
+            const m = this['lastID'];
+            db['get']('SELECT\x20*\x20FROM\x20Wallet\x20WHERE\x20user_id\x20=\x20?', [k['userId']], (n, o) => {
+                if (n) return b['status'](0x1f4)['json']({
+                    'error': 'Error\x20al\x20obtener\x20wallet'
+                }
+                );
+                const p = o['points'] + k['points'];
+                db['run']('UPDATE\x20Wallet\x20SET\x20points\x20=\x20?\x20WHERE\x20user_id\x20=\x20?', [p, k['userId']], function (q) {
+                    if (q) return b['status'](0x1f4)['json']({
+                        'error': 'Error\x20al\x20actualizar\x20wallet'
+                    }
+                    );
+                    db['run']('INSERT\x20INTO\x20Point_transactions\x20(user_id,\x20wallet_id,\x20amount_transaction,\x20type)\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20VALUES\x20(?,\x20?,\x20?,\x20?)', [k['userId'], o['id'], k['points'], 'add\x20ticket'], function (r) {
+                        if (r) return b['status'](0x1f4)['json']({
+                            'error': 'Error\x20al\x20insertar\x20ticket\x20history'
+                        }
+                        );
+                        return b['json']({
+                            'message': 'Ticket\x20subido\x20y\x20procesado\x20correctamente', 'fileName': g, 'text': h, 'points': i, 'status': j, 'ticketId': m, 'walletId': o['id'], 'newPoints': p
+                        }
+                        );
+
+                    }
+                    );
+
+                }
+                );
+
+            }
+            );
+
+        }
+        );
+
+    }
+    catch (l) {
+        return b['status'](0x1f4)['json']({
+            'error': l['message']
+        }
+        );
+
+    }
 
 }
-,'filename':(a,b,c)=>{
-try{
-const d=a['headers']['authorization'],e=d?.['split']('\x20')[0x1],f=decrypt(e),g=new Date(),h=g['getFullYear'](),i=String(g['getMonth']()+0x1)['padStart'](0x2,'0'),j=String(g['getDate']())['padStart'](0x2,'0'),k=String(g['getHours']())['padStart'](0x2,'0'),l=String(g['getMinutes']())['padStart'](0x2,'0'),m=String(g['getSeconds']())['padStart'](0x2,'0'),n=''+h+i+j+k+l+m,o=n+'_'+f+'.jpg';
-a['generatedFileName']=o,c(null,o);
+), router['get']('/mytickets', async (a, b) => {
+    const c = a['headers']['authorization'];
+    if (!c || !c['startsWith']('Bearer\x20')) return b['status'](0x191)['json']({
+        'error': 'Token\x20no\x20proporcionado\x20o\x20formato\x20inválido'
+    }
+    );
+    const d = c['split']('\x20')[0x1], e = decrypt(d);
+    if (!e) return b['status'](0x191)['json']({
+        'error': 'Token\x20inválido'
+    }
+    );
+    try {
+        db['all']('SELECT\x20*\x20FROM\x20Tickets\x20WHERE\x20user_id\x20=\x20?', [e], (f, g) => {
+            if (f) return b['status'](0x1f4)['json']({
+                'error': 'Error\x20al\x20obtener\x20tickets'
+            }
+            );
+            return b['json'](g);
+
+        }
+        );
+
+    }
+    catch (f) {
+        return b['status'](0x1f4)['json']({
+            'error': f['message']
+        }
+        );
+
+    }
 
 }
-catch(p){
-console['error']('Error\x20generando\x20nombre\x20de\x20archivo:',p),c(p);
-
-}
-
-}
-
-}
-),upload=multer({
-'storage':storage
-}
-),analyzeTicket=async a=>{
-try{
-const {
-data:{
-text:b
-}
-
-}
-=await Tesseract['recognize'](a,'spa',{
-'logger':c=>console['log'](c)
-}
-);
-return b;
-
-}
-catch(c){
-console['error']('Error\x20en\x20OCR:',c);
-throw new Error('Error\x20al\x20procesar\x20la\x20imagen\x20con\x20OCR');
-
-}
-
-}
-,calcularPuntos=a=>{
-let b=0x0;
-const c=/Total:\s*.*?(\d+(?:[.,]\d{
-1,2
-}
-)?)\s*€/i,d=a['match'](c);
-return d?b=parseFloat(d[0x1])*0x64:b=0x0,b;
-
-}
-;
-router['post']('/upload',upload['single']('imagen'),async(a,b)=>{
-const c=a['headers']['authorization'];
-if(!c||!c['startsWith']('Bearer\x20'))return b['status'](0x191)['json']({
-'error':'Token\x20no\x20proporcionado\x20o\x20formato\x20inválido'
-}
-);
-const d=c['split']('\x20')[0x1],e=decrypt(d);
-if(!e)return b['status'](0x191)['json']({
-'error':'Token\x20inválido'
-}
-);
-if(!a['file'])return b['status'](0x190)['json']({
-'error':'No\x20se\x20ha\x20subido\x20ninguna\x20imagen'
-}
-);
-const f=a['file']['path'],g=a['generatedFileName'];
-try{
-const h=await analyzeTicket(f),i=calcularPuntos(h);
-let j='ok';
-if(i===0x0)j='review';
-const k={
-'userId':e,'fileName':g,'text':h,'points':i
-}
-;
-db['run']('INSERT\x20INTO\x20Tickets\x20(user_id,\x20image_url,\x20json_content,\x20points_awarded,\x20status)\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20VALUES\x20(?,\x20?,\x20?,\x20?,\x20?)',[k['userId'],k['fileName'],k['text'],k['points'],j],function(l){
-if(l)return b['status'](0x1f4)['json']({
-'error':'Error\x20al\x20insertar\x20ticket'
-}
-);
-const m=this['lastID'];
-db['get']('SELECT\x20*\x20FROM\x20Wallet\x20WHERE\x20user_id\x20=\x20?',[k['userId']],(n,o)=>{
-if(n)return b['status'](0x1f4)['json']({
-'error':'Error\x20al\x20obtener\x20wallet'
-}
-);
-const p=o['points']+k['points'];
-db['run']('UPDATE\x20Wallet\x20SET\x20points\x20=\x20?\x20WHERE\x20user_id\x20=\x20?',[p,k['userId']],function(q){
-if(q)return b['status'](0x1f4)['json']({
-'error':'Error\x20al\x20actualizar\x20wallet'
-}
-);
-db['run']('INSERT\x20INTO\x20Point_transactions\x20(user_id,\x20wallet_id,\x20amount_transaction,\x20type)\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20VALUES\x20(?,\x20?,\x20?,\x20?)',[k['userId'],o['id'],k['points'],'add\x20ticket'],function(r){
-if(r)return b['status'](0x1f4)['json']({
-'error':'Error\x20al\x20insertar\x20ticket\x20history'
-}
-);
-return b['json']({
-'message':'Ticket\x20subido\x20y\x20procesado\x20correctamente','fileName':g,'text':h,'points':i,'status':j,'ticketId':m,'walletId':o['id'],'newPoints':p
-}
-);
-
-}
-);
-
-}
-);
-
-}
-);
-
-}
-);
-
-}
-catch(l){
-return b['status'](0x1f4)['json']({
-'error':l['message']
-}
-);
-
-}
-
-}
-),router['get']('/mytickets',async(a,b)=>{
-const c=a['headers']['authorization'];
-if(!c||!c['startsWith']('Bearer\x20'))return b['status'](0x191)['json']({
-'error':'Token\x20no\x20proporcionado\x20o\x20formato\x20inválido'
-}
-);
-const d=c['split']('\x20')[0x1],e=decrypt(d);
-if(!e)return b['status'](0x191)['json']({
-'error':'Token\x20inválido'
-}
-);
-try{
-db['all']('SELECT\x20*\x20FROM\x20Tickets\x20WHERE\x20user_id\x20=\x20?',[e],(f,g)=>{
-if(f)return b['status'](0x1f4)['json']({
-'error':'Error\x20al\x20obtener\x20tickets'
-}
-);
-return b['json'](g);
-
-}
-);
-
-}
-catch(f){
-return b['status'](0x1f4)['json']({
-'error':f['message']
-}
-);
-
-}
-
-}
-),module['exports']=router;
+), module['exports'] = router;
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
-const express=require('express');
+const express = require('express');
 require('dotenv')['config']();
-const cors=require('cors'),http=require('http'),userRoutes=require('./routes/userRoutes'),menuRoutes=require('./routes/menuRoutes'),reserveRoutes=require('./routes/reserveRoutes.js'),ticketsRoutes=require('./routes/ticketsRoutes.js'),marketRoutes=require('./routes/marketRoutes.js'),reseniasRoutes=require('./routes/reseniasRoutes.js'),adminRoutes=require('./routes/adminRoutes.js'),orderRoutes=require('./routes/orderRoutes.js'),mesasRoutes=require('./routes/mesasRoutes.js'),pdaRoutes=require('./routes/pdaRoutes.js'),app=express();
-app['use'](cors()),app['use'](express['json']()),app['use']('/api/user',userRoutes),app['use']('/api/menu',menuRoutes),app['use']('/api/reservas',reserveRoutes),app['use']('/api/tickets',ticketsRoutes),app['use']('/api/marketplace',marketRoutes),app['use']('/api/resenias',reseniasRoutes),app['use']('/api/admin',adminRoutes),app['use']('/api/orders',orderRoutes),app['use']('/api/mesas',mesasRoutes),app['use']('/api/pda',pdaRoutes),app['use']('/uploads',express['static']('uploads'));
-const PORT=process.env.PORT;
-app['listen'](PORT,()=>{
-console['log']('Servidor\x20escuchando\x20en\x20http://localhost:'+PORT);
+const cors = require('cors'), http = require('http'), userRoutes = require('./routes/userRoutes'), menuRoutes = require('./routes/menuRoutes'), reserveRoutes = require('./routes/reserveRoutes.js'), ticketsRoutes = require('./routes/ticketsRoutes.js'), reseniasRoutes = require('./routes/reseniasRoutes.js'), orderRoutes = require('./routes/orderRoutes.js'), mesasRoutes = require('./routes/mesasRoutes.js'), app = express();
+app['use'](cors()), app['use'](express['json']()), app['use']('/api/user', userRoutes), app['use']('/api/menu', menuRoutes), app['use']('/api/reservas', reserveRoutes), app['use']('/api/tickets', ticketsRoutes), app['use']('/api/resenias', reseniasRoutes), app['use']('/api/orders', orderRoutes), app['use']('/api/mesas', mesasRoutes), app['use']('/uploads', express['static']('uploads'));
+const PORT = process.env.PORT;
+app['listen'](PORT, () => {
+    console['log']('Servidor\x20escuchando\x20en\x20http://localhost:' + PORT);
 
 }
 );
