1da13df8ab0cbbb894aac828e5e56d90f75a635e
Sergio Salinas <correoSergio@falso.com>
2026-04-22 09:45:21 +0200

    feat(frontend): sidebar aparentemente correcto

--- frontend/src/Components/componenteDashboard/Sidebar.vue
+++ frontend/src/Components/componenteDashboard/Sidebar.vue
@@ -1,133 +1,87 @@
 <script setup>
 import { useRouter } from 'vue-router';
-import { ref, computed, onMounted, onUnmounted } from 'vue';
-import { HomeOutlined, UserOutlined, SettingOutlined, InfoCircleOutlined } from '@ant-design/icons-vue';
+import { ref, onMounted, onUnmounted } from 'vue';
+import { HomeOutlined, UserOutlined, HistoryOutlined, ShoppingOutlined, ShopOutlined, TagsOutlined } from '@ant-design/icons-vue';
 import './Sidebar.css';
 import { funcinalidadSidebar } from '../../Components/componenteDashboard/js/ocultarSidebar';
-const { cambiarEstadoSidebar,sidebarAbierto } = funcinalidadSidebar();
 
-defineProps({
-    collapsed: { type: Boolean, default: false }
-})
-const menuAbierto = ref (sidebarAbierto);
-const isCollapsed = computed(() => {
-    cambiarEstadoSidebar.value;
-    menuAbierto.value = sidebarAbierto;
-    
-    return sidebarAbierto;
-});
+const { cambiarEstadoSidebar, sidebarAbierto } = funcinalidadSidebar();
+const menuAbierto = ref(sidebarAbierto);
 const router = useRouter();
 
-const actualizarTamano = () => {
-    pantallaPeque.value = window.innerWidth < 768;
-}
-
 const pantallaPeque = ref(window.innerWidth < 768);
-
+const actualizarTamano = () => { pantallaPeque.value = window.innerWidth < 768; };
 onMounted(() => window.addEventListener('resize', actualizarTamano));
 onUnmounted(() => window.removeEventListener('resize', actualizarTamano));
 
-
+const rutasMenu = [
+    { key: '1', ruta: '/', label: 'Inicio', icon: HomeOutlined },
+    { key: '2', ruta: '/zonaPersonal', label: 'Zona Personal', icon: UserOutlined },
+    { key: '5', ruta: '/historial', label: 'Historial', icon: HistoryOutlined },
+    { key: '6', ruta: '/realizarPedido', label: 'Realizar Pedido', icon: ShoppingOutlined },
+    { key: '7', ruta: '/marketplace', label: 'Marketplace', icon: ShopOutlined },
+    {
+        key: 'sub1',
+        ruta: '',
+        label: 'Tickets',
+        icon: TagsOutlined,
+        subMenu: [
+            { key: '3', ruta: '/listarTickets', label: 'Mis Tickets' },
+            { key: '4', ruta: '/agregarTickets', label: 'Subir Ticket' },
+        ],
+    },
+];
 </script>
 
 <template>
-    <a-drawer v-if="pantallaPeque" placement="left" v-model:open="menuAbierto" :width="220" title="Menú">
-        <a-menu theme="light" mode="inline" :default-selected-keys="['1']">
-            <a-menu-item key="1" @click="router.push('/')">
-                <template #icon>
-                    <HomeOutlined />
-                </template>
-                Inicio
-            </a-menu-item>
 
-            <a-menu-item key="2" @click="router.push('/zonaPersonal')">
-                <template #icon>
-                    <UserOutlined />
-                </template>
-                Mi Perfil
-            </a-menu-item>
+    <a-drawer v-if="pantallaPeque" v-model:open="menuAbierto" placement="left" :width="220" title="Menú">
+        <div class="menuMovil">
+            <template v-for="entrada in rutasMenu" :key="entrada.key">
+                <!-- Entrada simple -->
+                <RouterLink v-if="!entrada.subMenu || !entrada.subMenu.length" :to="entrada.ruta"
+                    @click="cambiarEstadoSidebar">
+                    <component :is="entrada.icon" class="iconoMovil" />
+                    {{ entrada.label }}
+                </RouterLink>
 
-            <a-menu-item key="5" @click="router.push('/historial')">
-                <template #icon>
-                    <InfoCircleOutlined />
-                </template>
-                Historial
-            </a-menu-item>
-            <a-menu-item key="6" @click="router.push('/realizarPedido')">
-                <template #icon>
-                    <InfoCircleOutlined />
-                </template>
-                Realizar un pedido
-            </a-menu-item>
-            <a-menu-item key="7" @click="router.push('/marketplace')">
-                <template #icon>
-                    <InfoCircleOutlined />
-                </template>
-                Marketplace
-            </a-menu-item>
-            <a-sub-menu key="sub1">
-                <template #icon>
-                    <SettingOutlined />
-                </template>
-                <template #title>Tickets</template>
-
-                <a-menu-item key="3" @click="router.push('/listarTickets')">
-                    Mis Tickets
-                </a-menu-item>
-                <a-menu-item key="4" @click="router.push('/agregarTickets')">
-                    Subir Ticket
-                </a-menu-item>
-            </a-sub-menu>
-        </a-menu>
+                <div v-else class="grupoMovil">
+                    <span class="tituloGrupo">
+                        <component :is="entrada.icon" class="iconoMovil" />
+                        {{ entrada.label }}
+                    </span>
+                    <RouterLink v-for="subEntrada in entrada.subMenu" :key="subEntrada.key" :to="subEntrada.ruta"
+                        @click="cambiarEstadoSidebar">
+                        {{ subEntrada.label }}
+                    </RouterLink>
+                </div>
+            </template>
+        </div>
     </a-drawer>
 
-    <a-layout-sider v-else breakpoint="lg" :collapsed-width="0" :trigger="null" class="sidebarCustom">
+    <a-layout-sider v-else theme="light" breakpoint="lg" :collapsed-width="0" :trigger="null" class="sidebarCustom">
         <a-menu theme="light" mode="inline" :default-selected-keys="['1']" class="sidebarMenu">
-            <a-menu-item key="1" @click="router.push('/')">
-                <template #icon>
-                    <HomeOutlined />
-                </template>
-                Inicio
-            </a-menu-item>
+            <template v-for="entrada in rutasMenu" :key="entrada.key">
 
-            <a-menu-item key="2" @click="router.push('/zonaPersonal')">
-                <template #icon>
-                    <UserOutlined />
-                </template>
-                Mi Perfil
-            </a-menu-item>
-
-            <a-menu-item key="5" @click="router.push('/historial')">
-                <template #icon>
-                    <InfoCircleOutlined />
-                </template>
-                Historial
-            </a-menu-item>
-            <a-menu-item key="6" @click="router.push('/realizarPedido')">
-                <template #icon>
-                    <InfoCircleOutlined />
-                </template>
-                Realizar un pedido
-            </a-menu-item>
-            <a-menu-item key="7" @click="router.push('/marketplace')">
-                <template #icon>
-                    <InfoCircleOutlined />
-                </template>
-                Marketplace
-            </a-menu-item>
-            <a-sub-menu key="sub1">
-                <template #icon>
-                    <SettingOutlined />
-                </template>
-                <template #title>Tickets</template>
-
-                <a-menu-item key="3" @click="router.push('/listarTickets')">
-                    Mis Tickets
+                <a-menu-item v-if="!entrada.subMenu || !entrada.subMenu.length" :key="entrada.key"
+                    @click="router.push(entrada.ruta)">
+                    <template #icon>
+                        <component :is="entrada.icon" />
+                    </template>
+                    {{ entrada.label }}
                 </a-menu-item>
-                <a-menu-item key="4" @click="router.push('/agregarTickets')">
-                    Subir Ticket
-                </a-menu-item>
-            </a-sub-menu>
+
+                <a-sub-menu v-else>
+                    <template #icon>
+                        <component :is="entrada.icon" />
+                    </template>
+                    <template #title>{{ entrada.label }}</template>
+                    <a-menu-item v-for="subEntrada in entrada.subMenu" :key="subEntrada.key"
+                        @click="router.push(subEntrada.ruta)">
+                        {{ subEntrada.label }}
+                    </a-menu-item>
+                </a-sub-menu>
+            </template>
         </a-menu>
     </a-layout-sider>
 </template>
